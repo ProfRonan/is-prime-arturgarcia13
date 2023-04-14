42a3e4d57995fe482317b74c4feb692d2ee2c0ce
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-04-14 10:32:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if numInt <= 0 :
   print("Número inválido")
 
 else:
-    numerosControle = [2, 3, 4, 5, 6, 7, 8, 9]
+    numerosControle = [2, 3, 4, 5, 6, 7, 8, 9, 11]
     i = 0
     div = 0
     # coleta os divisores do numero
