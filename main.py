c3889d080ab61feba66c16fadb94e8327868e25e
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-04-14 11:00:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ if numInt <= 0 :
   print("Número inválido")
 
 else:
-    numerosControle = [2, 3, 4, 5, 6, 7, 8, 9, 11]
-    i = 0
+    i = 2
     div = 0
     # coleta os divisores do numero
-    while i < 8 :
-        if numInt % numerosControle[i] == 0:
+    while i <= (numInt**0.5) :
+        if numInt % i == 0 and numInt != (numInt**0.5):
             div += 1
+            if div >= 2:
+                break
         i += 1
     # número é primo
-    if div > 1 or numInt == 1:
+    if div > 0 or numInt == 1:
         print("Não primo")
     # testa se o número não é primo
-    elif div == 1 or div == 0:
+    elif div == 0:
         print("Primo")
